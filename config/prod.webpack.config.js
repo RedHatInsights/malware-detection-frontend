862a1cd8bd5156142f0243cd3b1be877c2155221
Allen Wight <awight@redhat.com>
2021-01-12 08:30:43 -0500

    Adds chrome 2.0 support. 🤪🤷‍♀️⏱ (#2)

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -4,6 +4,12 @@ const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
 
+plugins.push(
+    require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+        root: resolve(__dirname, '../')
+    })
+);
+
 module.exports = {
     ...webpackConfig,
     plugins
