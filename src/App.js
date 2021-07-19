701be0b77e7e048d835d5ea287e8534f9f073080
Allen Wight <awight@redhat.com>
2021-07-19 11:15:43 -0400

    MDA now supports global filter input
    fixes https://issues.redhat.com/browse/YARA-124

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
 
 import { NotificationsPortal, notifications } from '@redhat-cloud-services/frontend-components-notifications/';
-import React, { useEffect } from 'react';
 
 import { Banner } from '@patternfly/react-core/dist/esm/components/Banner/index';
 import { GET_MALWARE_COUNT } from './operations/queries';
+import React from 'react';
 import { Routes } from './Routes';
 import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
 import { hasMalware } from './store/cache';
@@ -18,15 +18,7 @@ registry.register({ notifications });
 const App = (props) => {
     const { data, loading } = useQuery(GET_MALWARE_COUNT);
     const intl = useIntl();
-
-    useEffect(() => {
-        insights.chrome.init();
-        insights.chrome.identifyApp('malware');
-        insights.chrome.hideGlobalFilter?.();
-    }, []);
-
     !loading && hasMalware(Number(data?.ruleStats?.matchedCount) !== 0);
-
     return <React.Fragment>
         <NotificationsPortal />
         { !loading && hasMalware() && <Banner variant="danger" className='ins-l-malwareBanner'>{intl.formatMessage(messages.weDetected)}</Banner>}
