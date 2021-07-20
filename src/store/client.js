450981a2fb122fd3a5dfbd46fb5ecc92f2796c0f
Allen Wight <awight@redhat.com>
2021-07-20 11:00:30 -0500

    MDA now supports global filter input (#13)
    fixes https://issues.redhat.com/browse/YARA-124

--- src/store/client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient } from '@apollo/client';
-import { cache } from './cache';
-
-const uri = `/api/malware-detection/v1/graphql`;
-
-const client = props => new ApolloClient({ cache, uri, ...props });
-
-export { client };
